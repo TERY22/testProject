3946f063775230f81dcfdf638799069791dbc622
TERY <fire_jacky@mail.ru>
2021-01-21 20:14:03 +0300

    modif js

--- js/main.js
+++ js/main.js
@@ -1,9 +1,7 @@
-const numberOfFilms = +prompt('How many movies do you watched?', '5'),
-      lastMovies    = prompt('What is last watched movies?', 'Avangers'),
-      rateMovies    = +prompt('Rate?', '10'),
-      lastMovies1   = prompt('What is last watched movies?', 'Avangers'),
-      rateMovies1   = +prompt('Rate?', '10');
+"use strict"; 
 
+const numberOfFilms = +prompt('How many movies do you watched?', '5');
+      
 const personalMovieDB = {
    count: numberOfFilms,
    movies: {},
@@ -12,8 +10,35 @@ const personalMovieDB = {
    privat: false,
 };
 
-personalMovieDB.movies[lastMovies] = rateMovies;
-personalMovieDB.movies[lastMovies1] = rateMovies1;
+if (personalMovieDB.count < 10) {
+   alert('Too less');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
+   alert('Not bad');
+} else if (personalMovieDB.count > 30) {
+   alert('You are cool');
+} else {
+   alert('Error');
+}
+
+for (let i = 0; i < 2; i++) {
+   const lastMovies    = prompt('What is last watched movies?', 'Avangers'),
+         rateMovies    = +prompt('Rate?', '10');
+
+   if (lastMovies != null && 
+      rateMovies  != null && 
+      lastMovies  != ''   && 
+      rateMovies  != ''   && 
+      lastMovies.length < 30) 
+   {
+      console.log('done');
+      personalMovieDB.movies[lastMovies] = rateMovies;
+   } else {
+      console.log('err');
+      i--;
+   } 
+}
+
+
 
 console.log(personalMovieDB);
 
